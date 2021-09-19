ab01f816ec3d02b6ad1839cc47d6fe50c052a2c8
william810212 <william810212@gmail.com>
2021-09-19 14:58:45 +0800

    chore: revise edit todo logic

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
 
 export default {
   name: "List",
@@ -13,7 +13,11 @@ export default {
   setup(props, { emit }) {
     const isEditing = ref(false);
 
-    const newContent = ref(props.content);
+    const newContent = ref(props.todo.content);
+
+    const editInput = ref(null);
+
+    const isCancel = ref(false);
 
     const updateState = (evt) => {
       emit("update-state", props.todo.id, evt.target.checked);
@@ -21,10 +25,27 @@ export default {
 
     const editTodo = () => {
       isEditing.value = true;
+
+      nextTick(() => {
+        editInput.value.focus();
+      });
     };
 
     const updateTodo = () => {
+      if (!isEditing.value) return;
+
+      if (isCancel.value) {
+        isCancel.value = false;
+        return;
+      }
+
       emit("update-todo", props.todo.id, newContent.value);
+      isEditing.value = false;
+    };
+
+    const cancelEdit = () => {
+      isCancel.value = true;
+      newContent.value = props.todo.content;
       isEditing.value = false;
     };
 
@@ -35,9 +56,12 @@ export default {
     return {
       isEditing,
       newContent,
+      editInput,
+      isCancel,
       updateState,
       editTodo,
       updateTodo,
+      cancelEdit,
       deleteTodo,
     };
   },
@@ -46,7 +70,7 @@ export default {
 
 <template>
   <li class="flex justify-between px-4 py-3 bg-white rounded-md">
-    <div class="flex items-center w-full space-x-3">
+    <div class="flex items-center w-full h-10 space-x-3">
       <input
         class="w-5 h-5 text-gray-800 rounded-full cursor-pointer focus:ring-0 active:scale-90"
         type="checkbox"
@@ -55,16 +79,19 @@ export default {
       />
       <div
         v-if="!isEditing"
-        class="w-full border-none px-2 text-gray-800 leading-10 focus:ring-0 focus:border"
+        class="w-full h-10 leading-10 border-none px-2 text-gray-800 focus:ring-0 focus:border"
         :class="todo.isDone ? 'line-through' : ''"
         @dblclick="editTodo"
       >{{ todo.content }}</div>
       <input
+        ref="editInput"
         v-if="isEditing"
-        v-model="newContent"
+        v-model.trim="newContent"
         class="w-full border-none px-2 rounded-md text-gray-800 focus:ring-gray-800"
         type="text"
+        @blur="updateTodo"
         @keydown.enter="updateTodo"
+        @keydown.esc="cancelEdit"
       />
     </div>
     <button
