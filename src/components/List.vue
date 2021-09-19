44b0d634f8a40762b17a49162a82786716559fd3
william810212 <william810212@gmail.com>
2021-09-19 15:17:42 +0800

    chore: update icon with heroicons

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,12 @@
 <script>
 import { ref, nextTick } from "vue";
+import { TrashIcon } from "@heroicons/vue/solid";
 
 export default {
   name: "List",
+  components: {
+    TrashIcon,
+  },
   emits: ["update-state", "update-todo", "delete-todo"],
   props: {
     todo: {
@@ -99,18 +103,7 @@ export default {
       type="button"
       @click="deleteTodo"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-4 w-4 fill-current"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      <TrashIcon class="h-4 w-4 fill-current" />
     </button>
   </li>
 </template>
